14e045aec2e8d55d62815de43ba5d47f8370966e
Abdur Rahim Nishad <nishad5089@gmail.com>
2021-02-09 18:08:01 +0600

    first commit

---
+++ src/app/meeting/meeting.module.ts
@@ -0,0 +1,17 @@
+import {NgModule} from '@angular/core';
+import {MeetingRoutingModule} from './meeting-routing.module';
+import {FormsModule} from '@angular/forms';
+import {CommonModule} from '@angular/common';
+import {DemoMaterialModule} from '../material.module';
+
+@NgModule({
+  imports: [
+    MeetingRoutingModule,
+    CommonModule,
+    FormsModule,
+    DemoMaterialModule,
+  ]
+})
+
+export class MeetingModule {
+}
